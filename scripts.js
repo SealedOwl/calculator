5fae1993f54384a2b0822c30532e6f1b184ae3da
SealedOwl <157732808+SealedOwl@users.noreply.github.com>
2024-04-22 22:26:20 +0530

    add display functions

--- scripts.js
+++ scripts.js
@@ -2,6 +2,23 @@ let exp1 = '';
 let operator = '';
 let exp2 = '';
 
+let display = '';
+const displayText = document.querySelector('.display-text');
+
+function displayScreen(exp){
+    //display operators and assign value to operator
+    if(exp === '+' || exp === '-' || exp === '*' || exp ==='/' || exp === '%'){
+        operator = exp;
+        displayText.textContent = exp;
+
+        //if an operator is clicked, then display exp2 next
+        display = '';
+    }else{
+        //display numbers
+        display+=exp;
+        displayText.textContent = display;
+    }
+}
 
 function add(num1, num2){
     num1 = Number(num1);
@@ -42,4 +59,29 @@ function operate(operator){
     if(operator === '%') result = modulus(exp1, exp2);
 
     return result;
+}
+
+function AC(){
+    display = '';
+    displayText.textContent = 0;
+}
+
+function DEL(){
+    if(display!==''){
+
+        let expArr = display.split("");
+        console.log(expArr);
+
+
+        lastChar = expArr.length-1;
+        console.log(lastChar);
+
+
+        expArr.splice(lastChar,1);
+        expArr = expArr.join('');
+        console.log(expArr);
+
+        display = expArr;
+        displayText.textContent = display;
+    }
 }
